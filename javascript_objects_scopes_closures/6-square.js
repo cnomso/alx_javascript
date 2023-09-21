0a836b1701c0cc6d00ee0aa4108557e88655f060
cnomso <cnomso@gmail.com>
2023-09-22 00:39:19 +0200

    task 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
 
-// a class square that inherits from class Rectangle 
+// a class square that inherits from class square of 5-square.js:
 
 const SquareFive = require('./5-square');
 
 class Square extends SquareFive {
   charPrint(c){
+    if (c === undefined) {
+        c = 'X';
+
     for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+        console.log(c.repeat(this.width));
         }
+    }
   }
 }
 
