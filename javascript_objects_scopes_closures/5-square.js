45ceaf13251f513f5a9b7e976e2b593b9639fad8
cnomso <cnomso@gmail.com>
2023-09-22 00:28:04 +0200

    task 5

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// a class square that inherits from class Rectangle 
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(w,h);
+  }
+}
+
+module.exports = Square;
