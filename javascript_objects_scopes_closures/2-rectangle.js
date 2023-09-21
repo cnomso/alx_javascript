018ffc9364b26938ae0c8695e9438c8fc5228eae
cnomso <cnomso@gmail.com>
2023-09-21 23:53:51 +0200

    task 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,12 +4,13 @@
 
 class Rectangle {
     constructor(w,h){
-        this.width = w;
-        this.height = h; 
-
         if (w <= 0 || h <= 0) {
             let object = {}
         }
+        this.width = w;
+        this.height = h; 
+
+     
     }
     
 }
