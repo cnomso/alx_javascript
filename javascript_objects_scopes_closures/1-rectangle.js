681ea66721af6f92b08a889e5a86b888184e99f2
cnomso <cnomso@gmail.com>
2023-09-21 23:10:26 +0200

    task 1

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+// a class Rectangle that defines a rectangle
+
+class Rectangle {
+    constructor(w,h){
+        this.width = w;
+        this.height = h; 
+    }
+}
+
+module.exports = Rectangle;
