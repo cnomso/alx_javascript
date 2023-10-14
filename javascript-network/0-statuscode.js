4d021600e043317618f0f9e8453cb4174e1d3ded
cnomso <cnomso@gmail.com>
2023-10-14 23:08:18 +0200

    status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -4,14 +4,8 @@ const request = require('request');
 request('http://www.google.com', (error, response, body) => {
     if (!error && response.statusCode === 200) {
         console.log('code: ' + response.statusCode);
-    }
-    if (!error && response.statusCode === 301) {
+    } else {
         console.log('code: ' + response.statusCode);
     }
-    if (!error && response.statusCode === 204) {
-        console.log('code:' + response.statusCode);
-    }
-    if (!error && response.statusCode === 404) {
-        console.log('code:' + response.statusCode);
-    }
+
 }); 
